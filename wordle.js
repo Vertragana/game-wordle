133c4397ed9fcdcca6fae57a3a2b58c6d58a9bd3
Vertragana <136757182+Vertragana@users.noreply.github.com>
2023-09-20 00:10:43 +0300

    save

--- wordle.js
+++ wordle.js
@@ -1,3 +1,10 @@
+const letterBox = document.querySelector(".letter-box");
+
+
+
+
+
+
 function isLetter(letter) {
-    return /^[a-zA-Z]$/.test(letter);
+    return /^[a-zA-Z]$/.test(letter);  // проверка является ли  стринга словом или нет
   }
